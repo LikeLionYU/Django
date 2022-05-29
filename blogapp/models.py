8998c30f9ab028e22fe7ad584ebdcf0a94481b72
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-05-29 15:49:25 +0900

    220529_ch4-18 Create Comments model & add it to admin

--- blogapp/models.py
+++ blogapp/models.py
@@ -11,3 +11,8 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE)
